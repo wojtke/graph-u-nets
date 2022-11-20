4ab1c3290bb57734b581ea173eddd532b463204c
Wojciech Jasiński <woj.jasinski@gmail.com>
2022-11-20 10:03:25 +0100

    improved training and model selection

--- model_selection.py
+++ model_selection.py
@@ -2,10 +2,11 @@ import argparse
 
 import optuna
 import torch
+from torch.utils.tensorboard import SummaryWriter
 
 from data_utils import get_dataloaders, get_dataset
 from nets import create_model
-from training import train_epoch, EarlyStopping
+from training import Trainer
 from utils import print_args
 
 
@@ -22,21 +23,21 @@ def define_objective(dataset, device, max_epochs, min_epochs, patience):
 
     def objective(trial):
         hyperparams = {
-            "batch_size": 64,
-            "learning_rate": 0.001,
-            "weight_decay": 1e-6,
+            "batch_size": trial.suggest_int("batch_size", 32, 128, step=32),
+            "learning_rate": trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True),
+            "weight_decay": trial.suggest_float("weight_decay", 1e-4, 5e-3, log=True),
 
             "channels_unet": trial.suggest_int("channels_unet", 256, 768, step=64),
-            "depth": 3,
-            "dropout_unet": 0.3,
+            "depth": trial.suggest_int("depth", 3, 4),
+            "dropout_unet": trial.suggest_float("dropout_unet", 0.0, 0.3),
             "activation_unet": "ELU",
-            "pool_ratios": 0.8,
+            "pool_ratios": trial.suggest_float("pool_ratios", 0.5, 0.9),
             "pooling": "TopKPooling",
 
-            "readout": "cat",
+            "readout": trial.suggest_categorical("readout", ["add", "mean", "max", "cat"]),
             "layers_classifier": 2,
-            "channels_classifier": 64,
-            "dropout_classifier": 0.3,
+            "channels_classifier": trial.suggest_int("channels_classifier", 64, 128, step=32),
+            "dropout_classifier": trial.suggest_float("dropout_classifier", 0.1, 0.3),
             "activation_classifier": "ELU"
         }
 
@@ -56,40 +57,30 @@ def define_objective(dataset, device, max_epochs, min_epochs, patience):
         )
         criterion = torch.nn.CrossEntropyLoss()
 
-        loss_history, acc_history = [], []
-        with EarlyStopping(
-                patience=patience, min_epochs=min_epochs, objective="maximize", verbose=True
-        ) as es:
-            for epoch in range(max_epochs):
-                loss, acc = train_epoch(
-                    model, optimizer, criterion, train_loader, val_loader, device
-                )
-                loss_history.append(loss)
-                acc_history.append(acc)
-                es.check(acc)
+        writer = SummaryWriter(f'runs/{trial.study.study_name}/trial{trial.number:03d}')
 
-                trial.report(acc, epoch)
+        trainer = Trainer(model, optimizer, criterion, device, writer=writer)
+        trainer.set_early_stopping(patience=patience, min_epochs=min_epochs, objective="acc")
+        trainer.set_optuna_trial_prunning(trial, objective="acc")
+        trainer.train(train_loader, val_loader, epochs=max_epochs)
 
-                if trial.should_prune():
-                    raise optuna.exceptions.TrialPruned()
-
-        return max(acc_history)
+        return trainer.get_best("val_acc")
 
     return objective
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Hyperparameter optimization")
-    parser.add_argument("--device", type=str, default="cpu", help="device")
+    parser.add_argument("--study-name", type=str, default="study5")
     parser.add_argument("--dataset", type=str, default="PROTEINS", help="dataset")
-    parser.add_argument("--max-epochs", type=int, default=300, help="max epochs")
+    parser.add_argument("--storage", type=str, default="sqlite:///storage.db")
+    parser.add_argument("--max-epochs", type=int, default=400, help="max epochs")
     parser.add_argument("--min-epochs", type=int, default=50, help="min epochs")
     parser.add_argument("--patience", type=int, default=50, help="early stopping")
-    parser.add_argument("--n-trials", type=int, default=100)
+    parser.add_argument("--n-trials", type=int, default=1000)
     parser.add_argument("--timeout", type=int, default=None)
+    parser.add_argument("--device", type=str, default="cpu", help="device")
     parser.add_argument("--n-jobs", type=int, default=1)
-    parser.add_argument("--study-name", type=str, default="study")
-    parser.add_argument("--storage", type=str, default="sqlite:///storage.db")
     args = parser.parse_args()
     return args
 
--- training.py
+++ training.py
@@ -1,78 +1,120 @@
+import numpy as np
+import optuna
 import torch
 
 
-def train_epoch(model, optimizer, criterion, train_loader, val_loader, device):
-    model.train()
+class Trainer:
+    def __init__(self, model, optimizer, criterion, device, writer=None, verbose=False):
+        self.model = model.to(device)
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
 
-    for batch in train_loader:
-        batch = batch.to(device)
-        out = model(batch.x, batch.edge_index, batch.batch)
-        loss = criterion(out, batch.y)
-        loss.backward()
-        optimizer.step()
+        self.epoch = 0
+        self.history = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
 
-    model.eval()
-    loss = 0
-    correct = 0
-    with torch.no_grad():
-        for batch in val_loader:
-            batch = batch.to(device)
-            out = model(batch.x, batch.edge_index, batch.batch)
-            loss += criterion(out, batch.y)
-            correct += (out.argmax(dim=1) == batch.y).sum()
-
-    loss = loss / len(val_loader.dataset)
-    acc = correct / len(val_loader.dataset)
-
-    return loss.cpu().item(), acc.cpu().item()
-
-
-class EarlyStopping:
-    def __init__(self, patience, objective="minimize", min_epochs=0, verbose=False):
-        self.objective = objective
-        self.patience = patience
-        self.min_epochs = min_epochs
+        self.writer = writer
         self.verbose = verbose
 
-        self.best_objective = None
-        self.best_epoch = None
-        self.epoch = 0
+        self.early_stopping = False
+        self.patience, self.min_epochs = None, None
+        self.best_epoch, self.best_value = None, None
+        self._early_stopping_check = None
 
-    def __enter__(self):
-        return self
+        self._optuna_callback = None
 
-    def __exit__(self, type, value, traceback):
-        return type == EarlyStoppingException
+    def set_early_stopping(self, patience, min_epochs=0, objective="loss"):
+        self.early_stopping = True
+        self.patience = patience
+        self.min_epochs = min_epochs
 
-    def check(self, objective):
-        objective = objective.item() if torch.is_tensor(objective) else objective
-        self.epoch += 1
-        if self.best_objective is None:
-            self.best_objective = objective
-            self.best_epoch = self.epoch
+        self.best_epoch = 1
+        self.best_value = np.inf if objective == "loss" else -np.inf
 
-        if (
-            self.objective == "minimize"
-            and objective < self.best_objective
-            or self.objective == "maximize"
-            and objective > self.best_objective
-        ):
-            self.best_objective = objective
-            self.best_epoch = self.epoch
+        if objective == "loss":
+            def check(val_loss, val_acc):
+                if val_loss < self.best_value:
+                    self.best_epoch = self.epoch
+                    self.best_value = val_loss
+        else:
+            def check(val_loss, val_acc):
+                if val_acc > self.best_value:
+                    self.best_epoch = self.epoch
+                    self.best_value = val_acc
 
-        if (
-            self.epoch - self.best_epoch > self.patience
-            and self.epoch > self.min_epochs
-        ):
+        self._early_stopping_check = check
+
+    def set_optuna_trial_prunning(self, trial, objective="loss"):
+        def optuna_callback(val_loss, val_acc):
+            trial.report(val_loss if objective == "loss" else val_acc, self.epoch)
+
+            if trial.should_prune():
+                raise optuna.exceptions.TrialPruned()
+
+        self._optuna_callback = optuna_callback
+
+    def get_best(self, key):
+        return self.history[key][self.best_epoch - 1]
+
+    def train(self, train_loader, val_loader, epochs):
+        while self.epoch < epochs:
+            self.epoch += 1
+            train_loss, train_acc, val_loss, val_acc = self.train_epoch(train_loader, val_loader)
             if self.verbose:
-                print(
-                    f"Early stopping after {self.epoch - 1} epochs with best "
-                    f"objective of {self.best_objective:.4f} at epoch {self.best_epoch}."
-                )
-            raise EarlyStoppingException()
+                print(f"Epoch {self.epoch}: train_loss={train_loss:.4f}, train_acc={train_acc:.4f}, val_loss={val_loss:.4f}, val_acc={val_acc:.4f}")
 
+            if self.writer:
+                self._write_to_history(train_loss, train_acc, val_loss, val_acc)
 
-class EarlyStoppingException(Exception):
-    """Exception to stop early."""
+            if self.early_stopping:
+                self._early_stopping_check(val_loss, val_acc)
+                if self.epoch - self.best_epoch > self.patience:
+                    break
 
-    pass
+            if self._optuna_callback:
+                self._optuna_callback(val_loss, val_acc)
+
+    def _write_to_history(self, train_loss, train_acc, val_loss, val_acc):
+        self.history["train_loss"].append(train_loss)
+        self.history["train_acc"].append(train_acc)
+        self.history["val_loss"].append(val_loss)
+        self.history["val_acc"].append(val_acc)
+
+        if self.writer:
+            self.writer.add_scalar('Loss/train', train_loss, self.epoch)
+            self.writer.add_scalar('Loss/test', val_loss, self.epoch)
+            self.writer.add_scalar('Accuracy/train', train_acc, self.epoch)
+            self.writer.add_scalar('Accuracy/test', val_acc, self.epoch)
+
+    def train_epoch(self, train_loader, val_loader):
+        self.model.train()
+
+        train_loss = 0
+        train_correct = 0
+        for batch in train_loader:
+            batch = batch.to(self.device)
+            out = self.model(batch.x, batch.edge_index, batch.batch)
+            loss = self.criterion(out, batch.y)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            train_loss += loss.item()
+            train_correct += out.argmax(dim=1).eq(batch.y).sum().item()
+
+        self.model.eval()
+        val_loss = 0
+        val_correct = 0
+        with torch.no_grad():
+            for batch in val_loader:
+                batch = batch.to(self.device)
+                out = self.model(batch.x, batch.edge_index, batch.batch)
+                val_loss += self.criterion(out, batch.y).item()
+                val_correct += out.argmax(dim=1).eq(batch.y).sum().item()
+
+        train_loss /= len(train_loader)
+        train_acc = train_correct / len(train_loader.dataset)
+        val_loss /= len(val_loader)
+        val_acc = val_correct / len(val_loader.dataset)
+
+        return train_loss, train_acc, val_loss, val_acc
