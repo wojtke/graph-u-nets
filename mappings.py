6a06834ac5c6cb552e61cf2a062e9f893eb9cf47
Wojciech Jasiński <woj.jasinski@gmail.com>
2022-11-30 00:46:25 +0100

    mappings for various methods

---
+++ mappings.py
@@ -0,0 +1,82 @@
+from typing import Callable
+
+import torch
+import torch_geometric
+import metrics
+import utils
+
+CONVOLUTION_METHODS = {
+    "gcn": torch_geometric.nn.conv.GCNConv,
+    "gat": torch_geometric.nn.conv.GATConv,
+    "sage": torch_geometric.nn.conv.SAGEConv,
+    "gin": torch_geometric.nn.conv.GINConv,
+}
+
+
+def get_conv(convolution_name: str) -> Callable:
+    """Get the convolution method by name."""
+    if convolution_name.lower() in CONVOLUTION_METHODS:
+        return CONVOLUTION_METHODS[convolution_name.lower()]
+    else:
+        raise ValueError(f"Convolution method '{convolution_name}' not supported.")
+
+
+POOLING_METHODS = {
+    "topk": torch_geometric.nn.TopKPooling,
+    "sag": torch_geometric.nn.SAGPooling,
+}
+
+
+def get_pool(pooling_name: str) -> Callable:
+    """Get the pooling method by name."""
+    if pooling_name.lower() in POOLING_METHODS:
+        return POOLING_METHODS[pooling_name.lower()]
+    else:
+        raise ValueError(f"Pooling method '{pooling_name}' not supported.")
+
+
+METRICS = {
+    "accuracy": metrics.Accuracy,
+    "auroc": metrics.AUROC,
+    "mse": metrics.MSE,
+}
+
+
+def get_evaluation_metric(metric_name: str) -> metrics.Metric:
+    """Get the metric by name."""
+    if metric_name.lower() in METRICS:
+        return METRICS[metric_name.lower()]
+    else:
+        raise ValueError(f"Metric '{metric_name}' not supported.")
+
+
+READOUT_METHODS = {
+    "mean": torch_geometric.nn.global_mean_pool,
+    "max": torch_geometric.nn.global_max_pool,
+    "add": torch_geometric.nn.global_add_pool,
+    "cat": utils.readout_cat
+}
+
+
+def get_readout(readout_name: str) -> Callable:
+    """Get the readout method by name."""
+    if readout_name.lower() in READOUT_METHODS:
+        return READOUT_METHODS[readout_name.lower()]
+    else:
+        raise ValueError(f"Readout method '{readout_name}' not supported.")
+
+
+ACTIVATIONS = {
+    "relu": torch.nn.ReLU,
+    "elu": torch.nn.ELU,
+    "leaky_relu": torch.nn.LeakyReLU,
+}
+
+
+def get_activation(activation_name: str) -> Callable:
+    """Get the activation function by name."""
+    if activation_name.lower() in ACTIVATIONS:
+        return ACTIVATIONS[activation_name.lower()]
+    else:
+        raise ValueError(f"Activation function '{activation_name}' not supported.")
+
